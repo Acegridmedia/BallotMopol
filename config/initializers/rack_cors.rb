3ed3afd549774082c1802afeca73a57ead9c3bc0
Sname <olumuyiwa.osiname@andela.com>
2016-10-12 19:58:41 +0100

    fix cross origin requests

--- config/initializers/rack_cors.rb
+++ config/initializers/rack_cors.rb
@@ -1,6 +1,14 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins '*'
+#     resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
+#   end
+# end
+Rails.application.config.middleware.insert_before 0, "Rack::Cors" do
   allow do
     origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
+    resource '*',
+      headers: :any,
+      methods: %i(get post put patch delete options head)
   end
 end
