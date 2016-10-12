744b042ca9a163f28af340004abc448d0ca29daa
Sname <olumuyiwa.osiname@andela.com>
2016-10-12 16:36:41 +0100

    remove items controller

--- app/controllers/v1/items_controller.rb
+++ app/controllers/v1/items_controller.rb
@@ -1,11 +1,8 @@
 class V1::ItemsController < ApplicationController
 
   def create
-    byebug
     render json: {message: "i am here"}, status: 201
   end
-
-  def
 
   # def item_params
   #   params.require(:item).permit(:name, :picture)
--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,6 @@ Rails.application.routes.draw do
       resources :comments, only: [:create, :destroy, :index]
     end
 
-    resources :items
-
 
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
