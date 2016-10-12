9685de7cb0065b6869b3df46f41a780db40c5727
Sname <olumuyiwa.osiname@andela.com>
2016-10-12 20:36:31 +0100

    move to config.ru

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,9 +5,9 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+Rails.application.config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
   allow do
-    origins 'localhost:8080'
+    origins '*'
 
     resource '*',
       headers: :any,
